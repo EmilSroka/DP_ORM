39a848f9b20fbbe81417ead8578f2fa86377c0bf
Emil Sroka <srokaemil@gmail.com>
2020-12-11 21:52:18 +0100

    test: setup test suite for Tables container
    created add,get,getName functions

--- library/src/main/metadata-containers/tables.ts
+++ library/src/main/metadata-containers/tables.ts
@@ -5,16 +5,19 @@ export class Tables {
   private nameToTableMap: Map<string, TableMap> = new Map();
 
   add(element: TableMap): void {
+    // TODO
     // should insert element into tableMaps and nameToTableMap
   }
 
   get(name: string): TableMap {
+    // TODO
     // should returns map with given name
     // if not exist -> throw error
     return {} as TableMap;
   }
 
   getNames(): string[] {
+    // TODO
     // should returns array of names of all tables
     return [];
   }
