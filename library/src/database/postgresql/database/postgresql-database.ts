fa0f71ff5f209833a43a62a0fbc246084f581fab
Emil Sroka <srokaemil@gmail.com>
2020-12-19 22:57:47 +0100

    fix: correct typo and add cb to catch
    refactor transaction method

--- library/src/database/postgresql/database/postgresql-database.ts
+++ library/src/database/postgresql/database/postgresql-database.ts
@@ -16,54 +16,26 @@ export class PostgresqlDatabase implements Database {
     await this.pool.end();
   }
 
-  // JS resources:
-  // 1. Promise
-  // -> https://www.youtube.com/watch?v=RvYYCGs45L4
-  // -> https://www.youtube.com/watch?v=FtdM3LIUXx4
-  // 2. async / await
-  // -> https://www.youtube.com/watch?v=gdOfDaX1BbU
-  // 3. if you want deep understanding
-  // -> https://www.youtube.com/watch?v=vn3tm0quoqE
-
-  // PostgreSQL resource: https://node-postgres.com/features/transactions -> A pooled client with async/await
-
   async transaction(
     actions: Array<(repository: PostgresqlRepository) => Promise<boolean>>,
   ): Promise<any> {
-    // TODO
-    // 0. if pool doesn't exist -> reject promise
-    // 1. get PoolClient from Pool
-    // 2. create PostgresqlRepository
-    // 2. start transaction: query 'BEGIN'
-    // 3. iterate over actions and invoke, pass created PostgresqlRepository as parameter
-    // -> if action fail (rejected promise or resolved as false): query 'ROLLBACK' -> reject promise (true)
-    // -> else (all actions resolves successfully) -> resolve promise (false)
+    if (this.pool == undefined) return Promise.reject(false);
 
-    if (typeof this.pool == 'undefined') {
-      return Promise.reject(); //new Error('Undefined type')
-    }
     const client = await this.pool.connect();
-
     try {
-      const pgrepository: PostgresqlRepository = new PostgresqlRepository(
-        client,
-      );
-
+      const repository = new PostgresqlRepository(client);
       await client.query('BEGIN');
-
       for (const action of actions) {
-        const result = await action(pgrepository);
-        if (!result) {
-          throw 'action failed';
-        }
+        const result = await action(repository);
+        if (result === false) throw 'action failed';
       }
-      return await Promise.resolve(false);
-    } catch (err) {
+      await client.query('COMMIT');
+    } catch {
       await client.query('ROLLBACK');
-      console.log(`Transaction failed ${err}`);
-      return await Promise.reject();
-    } finally {
-      client.release;
+      return Promise.reject(false);
     }
+
+    client.release();
+    return true;
   }
 }
