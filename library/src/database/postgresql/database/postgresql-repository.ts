f569483f7e0a29be29b2c6e32360ee194a96762c
DenisLyakhov <denislyakhov2015@gmail.com>
2021-01-06 20:44:16 +0100

    fix: simplify sql-insert algorithm

--- library/src/database/postgresql/database/postgresql-repository.ts
+++ library/src/database/postgresql/database/postgresql-repository.ts
@@ -1,31 +1,30 @@
 import { PoolClient } from 'pg';
-import any = jasmine.any;
 
 export class PostgresqlRepository {
   constructor(private client: PoolClient) {}
 
   insert(tableName: string, fields: string[], values: any[][]): Promise<any> {
-    let queryText = 'INSERT INTO ' + tableName + ' (' + fields[0];
-
-    for (let i = 1; i < fields.length; i++) {
-      queryText += ', ' + fields[i];
-    }
-
-    queryText += ') VALUES';
+    let queryText = `INSERT INTO ${tableName} (${fields.join(', ')})`;
 
     const flattenedValues: string[] = values.flat();
 
-    queryText += '($1';
+    queryText = `${queryText} VALUES`;
 
-    for (let i = 2; i <= flattenedValues.length; i++) {
-      if (i - 1 === values[0].length) {
-        queryText += '), ($' + String(i);
+    let index = 0;
+
+    for (let i = 0; i < values.length; i++) {
+      queryText += '($';
+      queryText += new Array(values[i].length)
+        .fill(1)
+        .map((_, j) => j + index + 1)
+        .join(', $');
+      if (i != values.length - 1) {
+        queryText += '), ';
+        index += values[i].length;
       } else {
-        queryText += ', $' + String(i);
+        queryText += ')';
       }
     }
-
-    queryText += ')';
 
     const queryObj = {
       text: queryText,
