89b22bcdb7b05359bb0d4861628d1b7fc350c360
DenisLyakhov <denislyakhov2015@gmail.com>
2021-01-06 14:06:42 +0100

    feat: sql-insert

--- library/src/database/postgresql/database/postgresql-repository.ts
+++ library/src/database/postgresql/database/postgresql-repository.ts
@@ -1,19 +1,37 @@
 import { PoolClient } from 'pg';
+import any = jasmine.any;
 
 export class PostgresqlRepository {
   constructor(private client: PoolClient) {}
 
   insert(tableName: string, fields: string[], values: any[][]): Promise<any> {
-    // TODO:
-    // 1. create "Query config object"
-    // - text: "INSERT INTO" query with proper:
-    //         - tableName and
-    //         - number of VALUES parts (length of values parameter),
-    //           each having correct $n for interpolation
-    //   check fixtures/postgresql-repository.ts for examples (if you want of course)
-    // - values: flattened values parameter
-    // https://node-postgres.com/features/queries#parameterized-query
-    // 2. call query on client field and return it's output (it should be a promise)
-    return Promise.reject();
+    let queryText = 'INSERT INTO ' + tableName + ' (' + fields[0];
+
+    for (let i = 1; i < fields.length; i++) {
+      queryText += ', ' + fields[i];
+    }
+
+    queryText += ') VALUES';
+
+    const flattenedValues: string[] = values.flat();
+
+    queryText += '($1';
+
+    for (let i = 2; i <= flattenedValues.length; i++) {
+      if (i - 1 === values[0].length) {
+        queryText += '), ($' + String(i);
+      } else {
+        queryText += ', $' + String(i);
+      }
+    }
+
+    queryText += ')';
+
+    const queryObj = {
+      text: queryText,
+      values: flattenedValues,
+    };
+
+    return this.client.query(queryObj);
   }
 }
